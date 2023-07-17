a04701835fd771dceafc37fc6eea5fe186c2c12b
Taimur Mumtaz <taimur.mumtaz0@gmail.com>
2023-07-17 13:03:36 -0400

    payment form UI fixed, now showing: TODO - redo servless functions to complete payment handler

--- src/Components/payment-form/payment-form.js
+++ src/Components/payment-form/payment-form.js
@@ -1,11 +1,15 @@
 import "./payment-form.styles.scss"
 import { CardElement } from "@stripe/react-stripe-js"
-import Button from '../button/button'
+import Button, {BUTTON_TYPE_CLASSES} from '../button/button'
 
 const PaymentForm = () => {
     return(
-        <div >
+        <div className="stripe-card-container" >
+            <div className="card-form">
+            <h2>Credit Card Payment</h2>
             <CardElement />
+            <Button buttonType={BUTTON_TYPE_CLASSES.inverted}>Pay Now</Button>
+            </div>
         </div>
     )
 }
